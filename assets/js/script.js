10a1b67165cb3b6c0e00b8a317e76e53d6b858fd
hilalkhullafaa <hilalzeppelin@gmail.com>
2024-08-14 12:55:46 +0700

    full section

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,21 @@ const addEventOnElement = function (el, type, callback) {
   }
 };
 
+// active animation header ketika scroll down 100px & bac top btn
+const header = document.getElementById("header");
+const backTopBtn = document.querySelector(".back-top-btn");
+const scrollActive = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+};
+
+addEventOnElement(window, "scroll", scrollActive);
+
 // navbar toggle
 const navbar = document.querySelector("[data-navbar]");
 const navToggler = document.querySelectorAll("[data-nav-toggler]");
@@ -20,8 +35,8 @@ const toggleNavbar = () => {
 addEventOnElement(navToggler, "click", toggleNavbar);
 
 // filter function
-const filterBtn = document.querySelectorAll("[data-filter-btn]");
-const filterItems = document.querySelectorAll("[data-filter]");
+const filterBtn = document.querySelectorAll(".filter-btn");
+const filterItems = document.querySelectorAll(".scrollbar-item");
 let lastClickBtn = filterBtn[0];
 
 const filter = function () {
@@ -30,12 +45,18 @@ const filter = function () {
   lastClickBtn = this;
 
   for (let i = 0; i < filterItems.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (filterItems[j].dataset.all === this.dataset.filterBtn) {
+        filterItems[j].style = "product-list .scrollbar";
+      }
+    }
     if (filterItems[i].dataset.filter === this.dataset.filterBtn) {
       filterItems[i].style.display = "block";
     } else {
       filterItems[i].style.display = "none";
     }
   }
+  return;
 };
 
 addEventOnElement(filterBtn, "click", filter);
